91fab2d7c8db21ca2885cf0451ef003569156e27
Mahadev Pillai <mxp5138@mavs.uta.edu>
2024-02-05 22:44:56 -0600

    P03 full credit

--- P03/Product.java
+++ P03/Product.java
@@ -16,10 +16,10 @@ public class Product {
 Double d1=Double.valueOf(price);
  return (String.format("%-" + width + "s", name)+ "$" + String.format("%5.2f",d1/100));
  } 
- private int getStockNumber(){
+ public int getStockNumber(){
 	 return stockNumber;
  }
- private int getPrice(){
+ public int getPrice(){
 	 return price;
  }
  
