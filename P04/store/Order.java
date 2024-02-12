cd3a58d25c2ceffaa45a4e6c20e03872054ef573
Mahadev Pillai <mxp5138@mavs.uta.edu>
2024-02-11 23:29:36 -0600

    P04 full credit

--- P04/store/Order.java
+++ P04/store/Order.java
@@ -1,16 +1,35 @@
 package store;
 import store.Item;
 import java.util.ArrayList;
-public class Order{
 
+/**
+* @author Mahadev Pillai
+* @version 0.2
+* @since 0.2
+* @license.agreement Gnu General Public License 3.0
+*/
+
+public class Order{
+/**
+* @param customer Customer type.
+* @since 0.2
+*/
 public Order(Customer customer){
 orderNumber = nextOrderNumber + 1;
 this.customer = customer;
 this.items=items;
 }
+/**
+* @param item Item type.
+* @since 0.2
+*/
 public void addItem(Item item){
 	items.add(item);
 }
+/**
+* @return int integer type
+* @since 0.2
+*/
 private int getPrice(){
 	int sum =0;
 	 for(Item i : items) {
@@ -19,11 +38,18 @@ private int getPrice(){
             }
 	 return sum;
  }
+ /**
+* @return int integer type
+* @since 0.2
+*/
  private int getOrderNumber(){
 	 return orderNumber;
  }
  
- 
+ /**
+* @return String string type
+* @since 0.2
+*/
 @Override
  public String toString() {
 	 Double d2=Double.valueOf(getPrice());
