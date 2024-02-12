7449a26e3261a1760c8a65f02aee23e87cf2e4f7
Mahadev Pillai <mxp5138@mavs.uta.edu>
2024-02-11 19:53:04 -0600

    P04 full credit

---
+++ P04/store/Plant.java
@@ -0,0 +1,8 @@
+package store;
+public class Plant extends Product{
+	Exposure exposure;
+	public Plant(String species, int price, Exposure exposure){		
+		super("Plant: " + species,price);
+		this.exposure=exposure;
+}
+}
